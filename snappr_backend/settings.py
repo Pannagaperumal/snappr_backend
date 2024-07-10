70eeb622c4195f36a69960167971e21b46e36a8e
Pannaga Perumal <pannagaperumal404@gmail.com>
2024-07-10 14:33:07 +0530

    Update settings.py

--- snappr_backend/settings.py
+++ snappr_backend/settings.py
@@ -50,10 +50,10 @@ MONGODB_DATABASES = {
         'NAME': 'Snappr_db',
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
-            'host': 'mongodb+srv://admin_snappr:3QXicaeVRNhrw5a@cluster0.iy3i2dt.mongodb.net/',
+            'host': '',
             'username': 'admin_snappr',
             'port':27017,
-            'password': '3QXicaeVRNhrw5a',
+            'password': '',
             'authMechanism': 'SCRAM-SHA-1'
         }
     }
